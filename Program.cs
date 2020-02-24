e2196ef58ecef891e8a93487f8cf9c06d4db8420
Andre Jann <default@web.de>
2020-02-24 11:18:08 +0100

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 5; i < 10; i= i + 2)
+            {
+                Console.WriteLine(i-2);
+                
+            }
+            
+        }
+    }
+}
