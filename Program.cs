ebfe8fff6eed35892d1d67c6623249c471820a70
AndreJann21 <61310553+AndreJann21@users.noreply.github.com>
2020-02-24 11:24:03 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -8,7 +8,8 @@ namespace ConsoleApp2
         {
             for (int i = 5; i < 10; i= i + 2)
             {
-                Console.WriteLine(i-2);
+                
+                Console.WriteLine(i-2)
                 
             }
             
